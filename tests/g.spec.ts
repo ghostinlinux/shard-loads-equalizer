8e495fd29bfe3c69a0bf778cd12ef938c3262302
Pratik Kumar <pratikkumar1701@gmail.com>
2024-11-16 18:52:16 +0530

    Added tags in Test Case

--- tests/g.spec.ts
+++ tests/g.spec.ts
@@ -1,110 +1,188 @@
 import { test } from "../baseTest";
 test.describe("g_1st_describe", () => {
-  test("confirm correct form submission works", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️confirm correct form submission works");
-  });
+  test(
+    "confirm correct form submission works",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️confirm correct form submission works");
+    }
+  );
 
-  test("validate user inputs and feedback", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️validate user inputs and feedback");
-  });
+  test(
+    "validate user inputs and feedback",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️validate user inputs and feedback");
+    }
+  );
 
-  test("check validation messages for errors", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️check validation messages for errors");
-  });
+  test(
+    "check validation messages for errors",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️check validation messages for errors");
+    }
+  );
 
-  test("ensure loading dynamic elements functions", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️ensure loading dynamic elements functions");
-  });
+  test(
+    "ensure loading dynamic elements functions",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️ensure loading dynamic elements functions");
+    }
+  );
 
-  test("assess performance under heavy load gaegyuoouiytsd", async ({
-    page,
-  }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️assess performance under heavy load gaegyuoouiytsd");
-  });
+  test(
+    "assess performance under heavy load gaegyuoouiytsd",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️assess performance under heavy load gaegyuoouiytsd");
+    }
+  );
 });
 
 test.describe("g_2nd_describe", () => {
-  test("verify responsive design on mobile", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️verify responsive design on mobile");
-  });
+  test(
+    "verify responsive design on mobile",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️verify responsive design on mobile");
+    }
+  );
 
-  test("check color contrast for accessibility", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️check color contrast for accessibility");
-  });
+  test(
+    "check color contrast for accessibility",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️check color contrast for accessibility");
+    }
+  );
 
-  test("test API response times effectively", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️test API response times effectively");
-  });
+  test(
+    "test API response times effectively",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️test API response times effectively");
+    }
+  );
 
-  test("assess database interactions for efficiency", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️assess database interactions for efficiency");
-  });
+  test(
+    "assess database interactions for efficiency",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️assess database interactions for efficiency");
+    }
+  );
 
-  test("ensure all user actions are logged", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️ensure all user actions are logged");
-  });
+  test(
+    "ensure all user actions are logged",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️ensure all user actions are logged");
+    }
+  );
 });
 
 test.describe("g_3rd_describe", () => {
-  test("validate application stability during testing", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️validate application stability during testing");
-  });
+  test(
+    "validate application stability during testing",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️validate application stability during testing");
+    }
+  );
 
-  test("check for proper error handling response", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️check for proper error handling response");
-  });
+  test(
+    "check for proper error handling response",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️check for proper error handling response");
+    }
+  );
 
-  test("review admin panel functionality thoroughly", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️review admin panel functionality thoroughly");
-  });
+  test(
+    "review admin panel functionality thoroughly",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️review admin panel functionality thoroughly");
+    }
+  );
 
-  test("ensure session timeouts are functional", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️ensure session timeouts are functional");
-  });
+  test(
+    "ensure session timeouts are functional",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️ensure session timeouts are functional");
+    }
+  );
 
-  test("test audit logs for issues effectively", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️test audit logs for issues effectively");
-  });
+  test(
+    "test audit logs for issues effectively",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️test audit logs for issues effectively");
+    }
+  );
 });
 
 test.describe("g_4th_describe", () => {
-  test("verify integration with external services", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️verify integration with external services");
-  });
+  test(
+    "verify integration with external services",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️verify integration with external services");
+    }
+  );
 
-  test("confirm user registration processes are smooth", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️confirm user registration processes are smooth");
-  });
+  test(
+    "confirm user registration processes are smooth",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️confirm user registration processes are smooth");
+    }
+  );
 
-  test("validate functionality of password resets", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️validate functionality of password resets");
-  });
+  test(
+    "validate functionality of password resets",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️validate functionality of password resets");
+    }
+  );
 
-  test("check localization and translation accuracy", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️check localization and translation accuracy");
-  });
+  test(
+    "check localization and translation accuracy",
+    { tag: "@one" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️check localization and translation accuracy");
+    }
+  );
 
-  test("ensure all hyperlinks are functional", async ({ page }) => {
-    await page.waitForTimeout(14000);
-    console.log("✔️ensure all hyperlinks are functional");
-  });
+  test(
+    "ensure all hyperlinks are functional",
+    { tag: "@two" },
+    async ({ page }) => {
+      await page.waitForTimeout(14000);
+      console.log("✔️ensure all hyperlinks are functional");
+    }
+  );
 });
